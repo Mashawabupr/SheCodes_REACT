c39b5be9280f1f468418718432e572585141899a
Mashawabupr <marikanik1999@gmail.com>
2022-07-07 13:28:39 +0300

    deleted some files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import React from "react";
+import Weather from "./weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1> Hello REACT!</h1>
+        <Weather />
       </header>
     </div>
   );
--- my-app/src/weather.js
+++ my-app/src/weather.js
@@ -3,9 +3,10 @@ import axios from "axios";
 
 export default function Weather() {
   function showWeather(response) {
-    console.log(response.data);
+    alert(response.data.main.temp);
   }
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=tbilisi&appid=2610fc391e59a1d4c413f050d38f672d`;
+  let url =
+    "https://api.openweathermap.org/data/2.5/weather?q=tbilisi&appid=2610fc391e59a1d4c413f050d38f672d";
   axios.get(url).then(showWeather);
   return <h2>Hello from weather!</h2>;
 }
